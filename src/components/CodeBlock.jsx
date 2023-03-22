cd50485c7b33f43ecd2fe40aec1041bce84a3299
Gholamreza Dar <rezadar1378@gmail.com>
2023-03-23 01:47:02 +0330

    Minify hljs
    load only python language

--- src/components/CodeBlock.jsx
+++ src/components/CodeBlock.jsx
@@ -1,5 +1,13 @@
-import SyntaxHighlighter from 'react-syntax-highlighter';
-import { irBlack } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
+// import SyntaxHighlighter from 'react-syntax-highlighter';
+// import { irBlack } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
+
+// Load only required languages
+// Don't forget to change /esm to /cjs
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
+import python from 'react-syntax-highlighter/dist/cjs/languages/hljs/python';
+import irBlack from 'react-syntax-highlighter/dist/cjs/styles/hljs/irBlack';
+
+SyntaxHighlighter.registerLanguage('python', python);
 
 const CodeBlock = ({ codeString }) => {
     return (
